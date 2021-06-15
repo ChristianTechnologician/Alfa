fda4f88a1372464c28096f750165d4abbf8b9974
Davide <davidecoticelli68@gmail.com>
2021-06-15 15:38:22 +0200

    DAO

--- src/main/java/Model/ConPool.java
+++ src/main/java/Model/ConPool.java
@@ -15,7 +15,7 @@ public class ConPool {
             p.setUrl("jdbc:mysql://localhost:3306/alfa?serverTimezone=" + TimeZone.getDefault().getID());
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
             p.setUsername("root");
-            p.setPassword("fabio0101");
+            p.setPassword("daviduccio15");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
